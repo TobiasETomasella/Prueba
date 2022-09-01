e8eba4aad7fdd240b8519da5bc30df52c1d99809
Tomasella Tobias <tobiastomasella@gmail.com>
2022-09-01 12:53:28 -0300

    cambios

--- index.js
+++ index.js
@@ -1,62 +1,83 @@
-
-const express = require('express'); //iniciar servidor
-const app = express(); // iniciar servidor
+// Requerimiento de Modulos
+const express = require ('express');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+//req: peticion
+//res: respuesta
+//res.json: respuesta en formato json
 
-app.listen(3000, () =>{ // iniciar servidor
-    console.log('servidor iniciado...');
-})
-
-app.get('/login', (req, res) => {
-    res.send(
-        `<html>
-        <head>
-            <tittle>Login</tittle>
-        </head>
-        <body>
-            <form method="POST"action="/api/auth">
-                Nombre de usuario: <input type ="user" name="user"><br/>
-                Contraseña: <input type = "password" name ="password"></br>
-                <input type="submit" value="Iniciar sesión" />
-            </form>
-        </body>
-    </html>`   
-    )
-
-app.post('/api/auth', (req, res) =>{
-
-    const usuario ={
-        user:req.body.user,
-        password: req.body.password
-    }
-    jwt.sign({usuario: usuario}, 'secretKey',(err, token) =>{
-        res.json({
-            token: token
-        })
-    })
-}
-)
+//ruta inicial
+app.get('/', ( req, res) => {
+    res.json({
+        text: 'la api funciona'
+    });
 });
 
-/*function validateToken(req, res, next){
+//ruta para logears
+app.post('/api/auth' , (req, res) => {
+    
+    //objeto use
+    const usuario = {
+        user: req.body.user,
+        password: req.body.password};
+    
+    if(req.body.user  === reverse(req.body.password)){
 
-    const accessToken = req.headers['Authorization'];
-
-    if(!accessToken) res.send('Acceso Denegado');
-
-    jwt.verify(accessToken, process.env.SECRET, (err, user) =>{
-        if(err){
-            res.send('Acceso Denegado, token expirado o incorrecto' );
-        }else{
-            req.user = user;
-            next();
+        const claim = {
+            user_id: 1234,
+            name: req.body.user
         }
-    });
-   
 
-}*/
+        const token = jwt.sign({claim}, 'my_secret_key',(err, token) =>{
+            //generacion del token
+    //sign: recibe el objeto del usuario
+    //my_secret_token: le permite al jwt cifrar descifrar el codigo
+    //
+            res.json({
+                token
+            });
+        });
 
+      
+    }
+
+
+});
+
+
+app.post('/api/protected',verifyToken, (req, res) =>{
+  jwt.verify(req.token, 'my_secret_key',(err, data) => {
+    if(err){
+        res.sendStatus(403);
+    } else {
+        res.json({
+            text: 'protected',
+            data: data
+        })
+    }
+  });
+});
+
+//Asegura que el token este creado
+function verifyToken(req, res, next){
+    const bearerHeader = req.headers['authorization']
+    if(typeof bearerHeader !== 'undefined'){
+       const bearerToken = bearerHeader.split(" ")[1]
+       req.token = bearerToken
+       next()
+    }else{
+        res.sendStatus(403)
+    }
+}
+
+function reverse(s){
+    return s.split("").reverse().join("")
+}
+
+//inicio de servidor
+app.listen(3000, () => {
+    console.log('Server iniciado...');
+});
